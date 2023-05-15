95eb8e72d76be936ecf51c54474751bffc457c1d
tolux <toluandfemi@gmail.com>
2023-05-15 13:07:43 -0700

    uuu

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -2,9 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AdminEntity } from './entities/admin.entity';
 import { Repository } from 'typeorm';
-// import { InvestmentPlanEntity } from 'src/invest/entities/investmentPlan.entity';
-// import { CreateInvestmentPlanDto } from 'src/invest/dto/create-investmentPlan.dto';
-// import { InvestService } from 'src/invest/invest.service';
 
 @Injectable()
 export class AdminService {
