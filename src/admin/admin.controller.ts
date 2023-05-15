c5e967cd5bfff0ac851d58ab7a9ecf3a05c8f118
tolux <toluandfemi@gmail.com>
2023-05-15 13:03:16 -0700

    update admin

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -18,24 +18,4 @@ import { updateMovieDto } from 'src/movie/dto/update-movie.dto';
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @Delete('/delete-movie/:id')
-  async deleteInvestmentPlan(@Param('id') id: string) {
-    // const data = await this.adminService.deleteInvestmentPlan(id);
-    // return { data, status: HttpStatus.OK, message: 'investment plan deleted' };
-  }
-  @Put('/update-movie')
-  async updateInvestmentPlan(@Body() updateMovieDto: updateMovieDto) {
-    // const data = await this.adminService.updateInvestmentPlan(
-    //   updateMovieDto,
-    // );
-    // return { data, status: HttpStatus.OK, message: 'investment plan updated' };
-  }
-
-  @Post('/create-movie')
-  async createInvestmentPlan(@Body() createMovieDto: createMovieDto) {
-    // const data = await this.adminService.createInvestmentPlan(
-    //   createMovieDto,
-    // );
-    // return { data, status: HttpStatus.OK, message: 'investment plan created' };
-  }
 }
